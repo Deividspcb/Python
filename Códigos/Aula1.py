674ce14fd194d03194efe7b2019f219f1be9c122
Deividspcb <deividpcb@gmail.com>
2024-06-16 15:20:55 -0300

    .

---
+++ Códigos/Aula1.py
@@ -0,0 +1,11 @@
+num1 = num2 = res = 0  # Inicialização das variáveis num1, num2 e res com o valor 0
+nome = "Deivid"  # Atribuição do valor "Deivid" à variável nome
+sobrenome = "Souza"  # Atribuição do valor "Souza" à variável sobrenome
+
+print(nome + " - " + sobrenome)  # Imprime na tela a concatenação das variáveis nome e sobrenome
+
+if 10 > 2:  # Verifica se 10 é maior que 2
+    print("Começando")  # Se a condição for verdadeira, imprime "Começando"
+    print("Aula2")  # Imprime "Aula2" após a condição verdadeira
+
+print("FIM")  # Imprime "FIM" independente da condição do if
